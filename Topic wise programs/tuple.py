b9d8d2e2ea57ef9bea251a8ace0867b7bce8dd18
Gaurav <ranswalgaurav44@gmail.com>
2026-03-05 20:18:08 +0530

    Add files via upload

---
+++ Topic wise programs/tuple.py
@@ -0,0 +1,7 @@
+'''• Tuples:- it is a built-in data type that let us create immutable sequence of values.
+• immutable & () is used.
+• index(element):- tuple.index(9).
+• count(element):- tuple.count(9).'''
+tuple=(5,6,0,11,10,11,99,11)
+print(tuple.index(99))
+print(tuple.count(11))
